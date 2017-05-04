51ce522983b5c2415b0b6939bf591b2bd5ba8e0b
alex <info@alexallmont.com>
2017-05-04 22:17:34 +0100

    Added Dijkstra shortest path implementation from chapter 3

---
+++ dijkstra.rb
@@ -0,0 +1,15 @@
+require_relative 'distance_grid'
+require_relative 'binary_tree'
+
+grid = DistanceGrid.new(5, 5)
+BinaryTree.on(grid)
+
+start = grid[0, 0]
+distances = start.distances
+
+grid.distances = distances
+puts grid
+
+puts 'path from northwest corner to southwest corner:'
+grid.distances = distances.path_to(grid[grid.rows - 1, 0])
+puts grid
---
+++ distances.rb
@@ -0,0 +1,38 @@
+class Distances
+  def initialize(root)
+    @root = root
+    @cells = {}
+    @cells[@root] = 0
+  end
+
+  def [](cell)
+    @cells[cell]
+  end
+
+  def []=(cell, distance)
+    @cells[cell] = distance
+  end
+
+  def cells
+    @cells.keys
+  end
+
+  def path_to(goal)
+    current = goal
+
+    breadcrumbs = Distances.new(@root)
+    breadcrumbs[current] = @cells[current]
+
+    until current == @root
+      current.links.each do |neighbour|
+        if @cells[neighbour] < @cells[current]
+          breadcrumbs[neighbour] = @cells[neighbour]
+          current = neighbour
+          break
+        end
+      end
+    end
+
+    breadcrumbs
+  end
+end
