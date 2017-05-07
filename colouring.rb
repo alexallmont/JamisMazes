4c0a8fd945740c5788228f026b9edbd6033bb10e
alex <info@alexallmont.com>
2017-05-07 18:30:18 +0100

    Added coloured grid rendering to conclude chapter 3

---
+++ colouring.rb
@@ -0,0 +1,13 @@
+require_relative 'coloured_grid'
+require_relative 'binary_tree'
+
+grid = ColouredGrid.new(25, 25)
+BinaryTree.on(grid)
+
+start = grid[grid.rows / 2, grid.columns / 2]
+
+grid.distances = start.distances
+
+filename = 'colourised.png'
+grid.to_png.save(filename)
+puts "Saved to #{filename}"
