4379e46e73507b3de904d2211fc3e25f38119978
alex <info@alexallmont.com>
2017-05-09 12:52:54 +0100

    Adding hunt and kill algorithm from chapter 5

---
+++ hunt_and_kill_demo.rb
@@ -0,0 +1,9 @@
+require_relative 'grid'
+require_relative 'hunt_and_kill'
+
+grid = Grid.new(20, 20)
+HuntAndKill.on(grid)
+
+filename = 'hunt_and_kill.png'
+grid.to_png.save(filename)
+puts "saving to #{filename}"
