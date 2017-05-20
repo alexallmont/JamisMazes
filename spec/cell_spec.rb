aacc6347daf93154a7079617be5a04def155fedd
alex <info@alexallmont.com>
2017-05-20 23:32:59 +0100

    converting test to new rspec syntax

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -8,18 +8,18 @@ describe Cell do
   describe '#new' do
     it 'returns a new Cell object' do
       c = Cell.new(0, 0)
-      c.should be_an_instance_of Cell
+      expect(c).to be_an_instance_of(Cell)
     end
 
     it 'should initialise empty' do
       c = Cell.new(0, 1)
-      c.row.should == 0
-      c.column.should == 1
-      c.north.should == nil
-      c.east.should == nil
-      c.south.should == nil
-      c.west.should == nil
-      c.links.should == []
+      expect(c.row).to eq(0)
+      expect(c.column).to eq(1)
+      expect(c.north).to eq(nil)
+      expect(c.east).to eq(nil)
+      expect(c.south).to eq(nil)
+      expect(c.west).to eq(nil)
+      expect(c.links).to eq([])
     end
   end
 end
