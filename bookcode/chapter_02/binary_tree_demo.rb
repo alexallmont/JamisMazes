9e5d05df6e768db9d8127c45d2251c72f238a81b
Alex Allmont <alexander.allmont@nanoporetech.com>
2018-04-12 10:23:03 +0100

    Adding code per-chapter, starting with initial example code

---
+++ bookcode/chapter_02/binary_tree_demo.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/ruby
+
+require_relative 'grid'
+require_relative 'binary_tree'
+
+grid = Grid.new(4, 4)
+BinaryTree.on(grid)
+
+puts grid
+
+puts "#{grid.deadends.count} dead-ends"
---
+++ bookcode/chapter_02/sidewinder_demo.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/ruby
+
+require_relative 'grid'
+require_relative 'sidewinder'
+
+grid = Grid.new(4, 4)
+Sidewinder.on(grid)
+
+img = grid.to_png
+img.save 'maze.png'
+puts 'saved to maze.png'
+
+puts "#{grid.deadends.count} dead-ends"
